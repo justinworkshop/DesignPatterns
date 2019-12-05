bda25b375158f253546ce4dea9b534c6db4a8012
wei.zheng <wei.zheng@ubtrobot.com>
2019-12-05 20:44:58 +0800

    策略模式UML示例代码

--- strategy/src/main/java/com/example/strategy/MainActivity.java
+++ strategy/src/main/java/com/example/strategy/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         mCalculatorContext = new CalculatorContext();
+
         mCalculatorContext.setStrategy(new BusStrategy());
         int busPrice = mCalculatorContext.calculatePrice(20);
 
