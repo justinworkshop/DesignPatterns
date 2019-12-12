59c8ff1005f9d91176f0a40721ea74aeb4bba6eb
wei.zheng <wei.zheng@ubtrobot.com>
2019-12-12 09:19:38 +0800

    添加责任链模式用户类

---
+++ iterator/src/main/java/com/example/iterator/uml/Client.java
@@ -0,0 +1,22 @@
+package com.example.iterator.uml;
+
+/**
+ * Copyright (C), 2016-2020
+ * FileName: Client
+ * Author: wei.zheng
+ * Date: 2019/12/11 21:16
+ * Description: 用户类
+ */
+public class Client {
+    public static void main(String[] args) {
+        ConcreteHandlerAA concreteHandlerAA = new ConcreteHandlerAA();
+
+        ConcreteHandlerBB concreteHandlerBB = new ConcreteHandlerBB();
+
+        concreteHandlerAA.setNextHandler(concreteHandlerBB);
+
+        concreteHandlerBB.setNextHandler(concreteHandlerAA);
+
+        concreteHandlerAA.handleRequest("ConcreteHandlerBB");
+    }
+}
